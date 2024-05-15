daa2de40b081baf62e5893013bcb7f00f3307d4b
soumyaathanimath <>
2024-05-15 20:40:44 +0530

    do while loop

---
+++ src/lloopp/Lloopp.java
@@ -0,0 +1,54 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lloopp;
+
+
+/**
+ *
+ * @author dell1
+ */
+public class Lloopp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int i=1;//100
+        /*while(i<100)
+        {
+            System.out.println(i);
+            i=i*2;
+        }*/
+        
+        /*do
+        {
+            System.out.println(i);
+            i=i*2;
+        }while(i<100);*/
+        
+        /*byte i=1;
+        while(true)
+        {
+            System.out.println(i);
+            i++;
+        }*/
+        
+        byte i=1;
+        
+        if(true)
+        {
+            System.out.println(i);
+            i++;
+        }
+        else
+            System.out.println("Bye");
+    }
+    
+}
+    
+    
+
